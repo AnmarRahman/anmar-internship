ab1820163b5435b561da1f7726728b01098a73d1
AnmaritoS <95002766+AnmaritoS@users.noreply.github.com>
2023-09-15 11:21:11 -0400

    Explore section NFT array has skeleton state loading while data is fetching

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import NftCard from "../common/NftCard";
+import Skeleton from "../UI/Skeleton";
 
 const ExploreItems = () => {
   const [nftArray, setNftArray] = useState([]);
@@ -35,15 +36,49 @@ const ExploreItems = () => {
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {nftArray.slice(0, nftsToShow).map((nft) => (
-        <div
-          key={nft?.id}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
-        >
-          <NftCard nft={nft} />
-        </div>
-      ))}
+      {nftArray.length > 0
+        ? nftArray.slice(0, nftsToShow).map((nft) => (
+            <div
+              key={nft?.id}
+              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+              style={{ display: "block", backgroundSize: "cover" }}
+            >
+              <NftCard nft={nft} />
+            </div>
+          ))
+        : Array.from({ length: 8 }).map((_, index) => (
+            <div
+              key={index}
+              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+              style={{ display: "block", backgroundSize: "cover" }}
+            >
+              <div className="nft__item">
+                <div className="author_list_pp">
+                  <Skeleton
+                    width={"60px"}
+                    height={"60px"}
+                    borderRadius={"25px"}
+                  />
+                  <i className="fa fa-check"></i>
+                </div>
+
+                <div className="nft__item_wrap">
+                  <Skeleton width={"100%"} height={"100%"} />
+                </div>
+
+                <div className="nft__item_info">
+                  <Skeleton width={"160px"} height={"20px"} marginTop={"4px"} />
+                  <div className="nft__item_price">
+                    <Skeleton width={"90px"} height={"20px"} />
+                  </div>
+                  <div className="nft__item_like">
+                    <Skeleton width={"28px"} height={"20px"} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+
       <div className="col-md-12 text-center">
         {nftsToShow < 16 && (
           <Link
